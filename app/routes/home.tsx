237ab187a5b3193fd6ddd0949b074165872e1ead
xApnea <jcbruno150@gmail.com>
2025-02-28 16:12:48 -0500

    Clean up tour date loading, WIP will need refactor later

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,9 @@
 import axios from "axios";
-import { DateTime } from 'luxon';
-import { Fragment } from 'react'
 
 import MailingListForm from "#app/components/mailingListForm.tsx";
 import Footer from "#app/components/navs/footer.tsx";
 import Header from "#app/components/navs/header.tsx";
+import TourDateTable from "#app/components/tourDateTable.tsx";
 import SocialIcons from "#app/components/ui/socials.tsx";
 import YoutubeEmbed from "#app/components/youtubeEmbed.tsx";
 
@@ -81,34 +80,7 @@ export default function Home({ loaderData, actionData }: Route.ComponentProps) {
       </section>
       <section id="tour" className="bg-green flex flex-col items-center py-8 md:py-10 animate-[fadeIn_2s;]">
         <h2 className="text-white text-center pb-10 text-5xl font-bold uppercase">Tour Dates</h2>
-        <div role="table" id="tourDatesTable" className="w-11/12 lg:w-7/8 2xl:w-[1500px] py-4 px-3 md:p-8 text-white text-xs sm:text-sm md:text-lg border-solid border-2 border-white/15 rounded-xl bg-white/10 backdrop-blur-sm grid grid-cols-3 gap-3 md:gap-4 items-center">
-          {
-            events.map((event: any, index: number) => {
-              const dt = DateTime.fromISO(event.datetime);
-              const date = dt.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY);
-
-              let callToAction;
-              if (event.offers.length === 0) {
-                callToAction = <button className="text-white font-bold uppercase py-1 px-2 md:text-base sm:px-4 md:px-8 justify-self-end bg-orange rounded-xl transition-all duration-300 ease-in-out shadow-none hover:shadow-orange/20 hover:shadow-md hover:scale-105" type="submit" onClick={() => { location.href = `${event.url}&trigger=notify_me`; }}>Notify</button>;
-              } else {
-                callToAction = <button className="text-white font-bold uppercase py-1 px-2 md:text-base sm:px-4 md:px-8 bg-orange rounded-xl transition-all duration-300 ease-in-out shadow-none hover:shadow-orange/20 hover:shadow-md hover:scale-105" type="submit" onClick={() => { location.href = event.offers[0].url; }}>Tickets</button>;
-              }
-
-              return (
-                <Fragment key={index}>
-                  <div className="justify-self-start text-left font-bold">{date}</div>
-                  <div className="justify-self-start text-left font-semibold">
-                    <p>{event.venue.location}</p>
-                    <p className="font-normal italic md:text-sm hidden md:inline">{event.venue.name}</p>
-                  </div>
-                  <div className="justify-self-center lg:justify-self-end text-center w-full md:w-1/2">
-                    {callToAction}
-                  </div>
-                </Fragment>
-              );
-            })
-          }
-            </div>
+        <TourDateTable events={events}/>
       </section>
       <section id="gallery" className="bg-[url('../assets/albumArtBackground.png')] bg-cover bg-center bg-no-repeat py-8 md:py-10 px-4 md:px-24">
         <div className="flex flex-col justify-center gap-8 sm:grid sm:grid-cols-4 sm:justify-items-center sm:place-content-center">
