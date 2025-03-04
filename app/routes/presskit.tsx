17d09b21aed58da13bc21b286a052cefa4d57f7e
xApnea <jcbruno150@gmail.com>
2025-03-04 12:47:25 -0500

    Update stats

--- app/routes/presskit.tsx
+++ app/routes/presskit.tsx
@@ -59,9 +59,9 @@ export default function PressKit() {
                 </a>
                 <div className="flex flex-col justify-end h-full gap-1">
                   <h4 className="font-normal text-base">Followers</h4>
-                  <p className="font-bold text-5xl">5k+</p>
+                  <p className="font-bold text-5xl">5300+</p>
                   <h4 className="font-normal text-base">“Nowhere Else to Go”</h4>
-                  <p className="font-bold text-5xl">690k+</p>
+                  <p className="font-bold text-5xl">695k+</p>
                   <h4 className="font-normal text-base">“Blood Moon”</h4>
                   <p className="font-bold text-5xl">145k+</p>
                 </div>
@@ -76,7 +76,7 @@ export default function PressKit() {
                 <div className="flex flex-col justify-end h-full gap-1">
                   <h3 className="sr-only">Instagram</h3>
                     <h4 className="font-normal text-base">Followers</h4>
-                    <p className="font-bold text-5xl">7800+</p>
+                    <p className="font-bold text-5xl">8000+</p>
                 </div>
               </div>
             </div>
@@ -89,9 +89,9 @@ export default function PressKit() {
                 <div className="flex flex-col justify-end h-full gap-1">
                   <h3 className="sr-only">TikTok</h3>
                     <h4 className="font-light text-base">Followers</h4>
-                    <p className="font-bold text-5xl">9800+</p>
+                    <p className="font-bold text-5xl">9900+</p>
                     <h4 className="font-normal text-base">Likes</h4>
-                    <p className="font-bold text-5xl">575k+</p>
+                    <p className="font-bold text-5xl">595k+</p>
                 </div>
               </div>
             </div>
