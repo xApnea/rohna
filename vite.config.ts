0fb4192a9b51281a3f1626e4ddcfcc5db5e5b008
xApnea <jcbruno150@gmail.com>
2025-05-22 23:27:42 -0400

    Linting fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { reactRouter } from '@react-router/dev/vite'
 import { sentryVitePlugin } from '@sentry/vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 import { reactRouterDevTools } from 'react-router-devtools'
-import tailwindcss from "@tailwindcss/vite";
 import { type UserConfig } from 'vite'
 
 import { envOnlyMacros } from 'vite-env-only'
